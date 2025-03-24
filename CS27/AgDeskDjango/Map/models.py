e0f6c24ef8538fc65f2fb2fa259948e8e417637a
AnS-2000 <146499661+AnS-2000@users.noreply.github.com>
2025-03-24 19:49:01 +1100

    database

--- CS27/AgDeskDjango/Map/models.py
+++ CS27/AgDeskDjango/Map/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.gis.db import models  # ✅ GeoDjango
+from django.utils import timezone
 
-# Create your models here.
+from FarmAcc.models import FarmInfo
+
+
+class NDVIRegion(models.Model):
+    farm = models.ForeignKey(FarmInfo, on_delete=models.CASCADE)
+    geometry = models.PolygonField(srid=4326)
+    image = models.ImageField(upload_to='ndvi_images/')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"NDVIRegion {self.id} - {self.farm.farm_name}"
