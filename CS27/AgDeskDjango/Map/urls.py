2ef569fefdc71a14e27c9018f02b46f95b7f69e2
AnS-2000 <146499661+AnS-2000@users.noreply.github.com>
2025-03-24 20:32:14 +1100

    front end

--- CS27/AgDeskDjango/Map/urls.py
+++ CS27/AgDeskDjango/Map/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.map_view, name='map'),
-    path('api/ndvi/', views.ndvi_view, name='ndvi_view')
+    path('api/ndvi/', views.ndvi_view, name='ndvi_view'),
+    path('api/save-ndvi/', views.save_ndvi_result, name='save_ndvi_result')
 ]
