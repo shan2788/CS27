6549d3a527af0ff9a5289fcea6db95305bb7ba9e
AnS-2000 <146499661+AnS-2000@users.noreply.github.com>
2025-04-20 20:12:10 +1000

    add history comparison function

--- CS27/AgDeskDjango/Map/urls.py
+++ CS27/AgDeskDjango/Map/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('recommend-tree/', views.tree_recommendation_view, name='tree-recommendation'),
     path('api/region-history/<int:farm_id>/', views.region_history, name='region_history'),
     path('api/report-history/<int:farm_id>/', views.report_history, name='report_history'),
+    path("api/ndvi-monthly/", views.ndvi_monthly_summary, name="ndvi_monthly_summary"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
