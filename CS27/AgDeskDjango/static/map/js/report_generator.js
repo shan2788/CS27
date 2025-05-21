96711ab9a93476c6ebfd1c8be5e3d493e4c91fa7
AnS-2000 <146499661+AnS-2000@users.noreply.github.com>
2025-05-22 01:28:22 +1000

    fix the bug

--- CS27/AgDeskDjango/static/map/js/report_generator.js
+++ CS27/AgDeskDjango/static/map/js/report_generator.js
@@ -75,3 +75,50 @@ document.getElementById("carbon-credit-btn").addEventListener("click", function 
       console.error(err);
     });
 });
+
+document.getElementById("tree-recommendation-btn").addEventListener("click", function () {
+  if (!selectedGeometry) {
+    alert("Please draw a region first.");
+    return;
+  }
+  const farmDetails = document.getElementById("farm-details");
+
+  const farmData = {
+    farmId: farmDetails.dataset.farmId,
+    farmName: farmDetails.dataset.farmName,
+    farmLocation: farmDetails.dataset.farmLocation
+  };
+
+  showLoading("Generating Tree Recommendation...");
+
+  const payload = {
+    geometry: selectedGeometry,
+    start_date: document.getElementById("start-date").value,
+    end_date: document.getElementById("end-date").value,
+    farm_details: farmData,
+    farm_id: farmData.farmId
+  };
+  console.log(payload.farm_id)
+
+  fetch("/map/recommend-tree/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrfToken
+    },
+    body: JSON.stringify(payload)
+  })
+    .then(res => {
+      if (!res.ok) throw new Error("Network error");
+      return res.text();  // 后端返回 HTML 部分
+    })
+    .then(html => {
+      hideLoading();
+      document.getElementById("tree-recommendation-result").innerHTML = html;
+    })
+    .catch(err => {
+      hideLoading();
+      console.error(err);
+      alert("Failed to fetch tree recommendation.");
+    });
+});
